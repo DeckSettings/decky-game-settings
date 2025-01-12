afc1edcb46f823781b71da4af6eed08e2b108bfa
Josh.5 <jsunnex@gmail.com>
2025-01-12 22:58:55 +1300

    I want to add a search feature later on...

--- src/components/QuickAccessView.tsx
+++ src/components/QuickAccessView.tsx
@@ -6,14 +6,47 @@ import GameDataView from "./GameDetailsView";
 
 const QuickAccessView: React.FC = () => {
 
-    const [selectedGame, setSelectedGame] = useState<GameInfo | null>(null);
+    //const setShowSettings = () => {
+    //    console.log(`Show Plugin Settings`);
+    //};
 
+    //const onSearchModalClosed = async (query: string) => {
+    //    Navigation.OpenQuickAccessMenu(QuickAccessTab.Decky);
+    //    onSearch(query);
+    //}
+
+    const [selectedGame, setSelectedGame] = useState<GameInfo | null>(null);
     const handleGameSelect = (game: GameInfo) => {
         setSelectedGame(game);
     };
 
     return (
         <div>
+            {/*<PanelSection>
+                <PanelSectionRow>
+                    <ButtonItem
+                        layout="below"
+                        onClick={() => setShowSettings()}
+                    >
+                        Go To Settings
+                    </ButtonItem>
+                </PanelSectionRow>
+            </PanelSection>
+            <hr/>*/}
+            {/*<PanelSection>
+                <PanelSectionRow>
+                    <Focusable>
+                        <TextField
+                            label="Search for games"
+                            // @ts-ignore
+                            placeholder={'Search for game by name or appid'}
+                            onClick={() => showModal(<TextFieldModal label="Search" placeholder="Search for game by name or appid" onClosed={onSearchModalClosed} />)}
+                        />
+                    </Focusable >
+                </PanelSectionRow>
+            </PanelSection>
+            <hr/>*/}
+
             {selectedGame ? (
                 <GameDataView
                     gameName={selectedGame.title}
