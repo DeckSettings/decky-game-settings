55a0dda61caeffa6cf7215c9bb2afcaa66fb25ec
Josh.5 <jsunnex@gmail.com>
2025-01-12 23:18:40 +1300

    Update metadata for plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,30 @@
 import {
-  staticClasses
+    staticClasses
 } from "@decky/ui";
 import {
-  definePlugin,
-  // routerHook
+    definePlugin,
+    // routerHook
 } from "@decky/api"
-import { FaShip } from "react-icons/fa";
+import {MdTune} from "react-icons/md";
 import QuickAccessView from "./components/QuickAccessView";
 
 // import logo from "../assets/logo.png";
 
 export default definePlugin(() => {
-  console.log("Plugin initializing, this is called once on frontend startup")
+    console.log("Plugin initializing, this is called once on frontend startup")
 
-  return {
-    // The name shown in various decky menus
-    name: "DeckyGameSettings",
-    // The element displayed at the top of your plugin's menu
-    titleView: <div className={staticClasses.Title}>Game Settings</div>,
-    // The content of your plugin's menu
-    content: <QuickAccessView />,
-    // The icon displayed in the plugin list
-    icon: <FaShip />,
-    // The function triggered when your plugin unloads
-    onDismount() {
-      console.log("Unloading")
-    },
-  };
+    return {
+        // The name shown in various decky menus
+        name: "DeckyGameSettings",
+        // The element displayed at the top of your plugin's menu
+        titleView: <div className={staticClasses.Title}>Deck Settings</div>,
+        // The content of your plugin's menu
+        content: <QuickAccessView/>,
+        // The icon displayed in the plugin list
+        icon: <MdTune/>,
+        // The function triggered when your plugin unloads
+        onDismount() {
+            console.log("Unloading")
+        },
+    };
 });
